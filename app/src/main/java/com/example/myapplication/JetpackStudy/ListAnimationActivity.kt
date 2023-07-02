fcdf38758d26afb8a6c1218acde60f53a48804df
김진우 <jinwoo3661@naver.com>
2023-07-02 13:25:31 +0900

    compose 적용

--- app/src/main/java/com/example/myapplication/JetpackStudy/ListAnimationActivity.kt
+++ app/src/main/java/com/example/myapplication/JetpackStudy/ListAnimationActivity.kt
@@ -1,43 +1,62 @@
 package com.example.myapplication.JetpackStudy
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.JetpackStudy.ui.theme.MyApplicationTheme
-import com.example.myapplication.JetpackStudy.Message
 import com.example.myapplication.R
 
-class JetPackLayoutActivity : ComponentActivity() {
+// Compose를 활용해서 목록과 애니메이션 추가
+// UI 상태를 저장하기 위해서 메시지 확장을 추적해야 함
+// 이 추적 기능을 구현하기 위해서 remember와 mutableStateOf 함수 사용
+class ListAnimationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // 테마는 프로젝트 이름을 기준으로 생성된다.
-            // 예제에서는 포로젝트 이름을 ComposeTutorial르 횄기 때문에
-            // ComposeTutorialTheme가 적용 된다.
-            MyApplicationTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(msg = Message("Android","Jetpack Compose"))
-                }
-            }
+            PreviewConversation()
         }
     }
 }
 
 @Composable
-fun MessageCard(msg:Message) {
+fun Conversation(messages : List<Message>){
+    LazyColumn{
+        items(messages){message->
+            MessageCard2(msg = message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation(){
+    // 제작해둔 테마 사용
+    MyApplicationTheme {
+        Conversation(messages = SampleData.conversationSample)
+    }
+}
+
+// 애니메이션 적용을 위한 메시지 카드 2
+@Composable
+fun MessageCard2(msg:Message) {
     // 디자인 구조를 위한 Row 스코프 추가
 
     // 메시지 근처에 패딩 추가
@@ -56,8 +75,18 @@ fun MessageCard(msg:Message) {
         // horizontal 공간 추가 (이미지와 콜럼 사이)
         Spacer(modifier = Modifier.width(8.dp))
 
+        // 메시지 확장 여부를 추적하기 위한 변수 (remember로 ui 상태 기억)
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfacecolor를 적용 : 다른 색상으로 점진적 변화
+        // primary -> surface로 점진적 수정 배경 애니메이션 적용
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary
+            else MaterialTheme.colors.surface
+        )
         // 수직 정렬
-        Column {
+        //isExpanded가 클릭 상태인지 확인
+        // 이 column을 클릭하면, isExpanded 변수를 토글(확장)함
+        Column (modifier = Modifier.clickable { isExpanded=!isExpanded }){
             Text(
                 text = msg.author,
                 // 매핑 된 색상 변경  :Surface를 활용한 테마 변경
@@ -69,30 +98,22 @@ fun MessageCard(msg:Message) {
             Spacer(modifier = Modifier.height(4.dp))
 
             // 텍스트에 도형 추가 (텍스트를 감싸는 이미지
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium, elevation = 1.dp,
+                color = surfaceColor, // 위에서 적용한 점진적 애니메이션 칼라 적용
+                // 점차적인 표면 크기 변화 적용 (1dp)
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // 메세지가 확장 상태라면 모든 메시지를 보여주고
+                    // 확장 상태가 아니라면 1줄만 보여주기
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
 
-        }
-    }
-}
-
-// preview도 테마에 맞게 수정
-@Preview
-@Composable
-fun PreviewMessageCard2() {
-    MyApplicationTheme{
-        Surface{
-            MessageCard(
-                msg = Message(
-                    "Colleague",
-                    "Hey, take a look at Jetpack Compose, it's great!"
-                )
-            )
         }
     }
 }
